261eb0e93b66101bafa90ee02e49b6046440ff68
Sasha Joseph <sasha@fuzzproductions.com>
2014-12-08 00:27:24 -0500

    Change all require to require_once

--- AuthorizeNet.php
+++ AuthorizeNet.php
@@ -4,20 +4,20 @@
  *
  * @package AuthorizeNet
  */
-require dirname(__FILE__) . '/lib/shared/AuthorizeNetRequest.php';
-require dirname(__FILE__) . '/lib/shared/AuthorizeNetTypes.php';
-require dirname(__FILE__) . '/lib/shared/AuthorizeNetXMLResponse.php';
-require dirname(__FILE__) . '/lib/shared/AuthorizeNetResponse.php';
-require dirname(__FILE__) . '/lib/AuthorizeNetAIM.php';
-require dirname(__FILE__) . '/lib/AuthorizeNetARB.php';
-require dirname(__FILE__) . '/lib/AuthorizeNetCIM.php';
-require dirname(__FILE__) . '/lib/AuthorizeNetSIM.php';
-require dirname(__FILE__) . '/lib/AuthorizeNetDPM.php';
-require dirname(__FILE__) . '/lib/AuthorizeNetTD.php';
-require dirname(__FILE__) . '/lib/AuthorizeNetCP.php';
+require_once dirname(__FILE__) . '/lib/shared/AuthorizeNetRequest.php';
+require_once dirname(__FILE__) . '/lib/shared/AuthorizeNetTypes.php';
+require_once dirname(__FILE__) . '/lib/shared/AuthorizeNetXMLResponse.php';
+require_once dirname(__FILE__) . '/lib/shared/AuthorizeNetResponse.php';
+require_once dirname(__FILE__) . '/lib/AuthorizeNetAIM.php';
+require_once dirname(__FILE__) . '/lib/AuthorizeNetARB.php';
+require_once dirname(__FILE__) . '/lib/AuthorizeNetCIM.php';
+require_once dirname(__FILE__) . '/lib/AuthorizeNetSIM.php';
+require_once dirname(__FILE__) . '/lib/AuthorizeNetDPM.php';
+require_once dirname(__FILE__) . '/lib/AuthorizeNetTD.php';
+require_once dirname(__FILE__) . '/lib/AuthorizeNetCP.php';
 
 if (class_exists("SoapClient")) {
-    require dirname(__FILE__) . '/lib/AuthorizeNetSOAP.php';
+    require_once dirname(__FILE__) . '/lib/AuthorizeNetSOAP.php';
 }
 /**
  * Exception class for AuthorizeNet PHP SDK.
